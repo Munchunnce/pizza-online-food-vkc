97989166d458809059ecfa4590379060f5b6f23e
Vimal <vimalkumarak478@gmail.com>
2025-09-26 21:45:20 +0530

    navbar create and design vkc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter, Route, Routes,  } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>welcome to pizza-food.</h1>
+      <BrowserRouter>
+      <Navigation/>
+        <Routes>
+          <Route path='/' exact element={<Home/>}></Route>
+          {/* <Route path='/cart' element={<Cart/>}></Route> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Navigation = () => {
+
+
+  const cartStyle = {
+    background: '#F59E0D',
+    display: 'flex',
+    padding: '6px 12px',
+    borderRadius: '50px',
+  }
   return (
-    <div>
-      This is Navbar.
-    </div>
+    <>
+      <nav className='container max-auto flex items-center justify-between py-4 m-auto'>
+        <Link to='/'>
+          <img style={{ height: 45 }} src="/images/logo.png" alt="logo" />
+        </Link>
+        <ul className='flex items-center'>
+          <li><Link to='/'>Home</Link></li>
+          <li className='ml-6'><Link to='/products'>Products</Link></li>
+          <li className='ml-6'><Link to='/register'>SignIn</Link></li>
+          <li className='ml-6'><Link to='/login'>Login</Link></li>
+          <li className='ml-6'>
+            <Link to='/cart'>
+              <div style={cartStyle}>
+                <span>10</span>
+                <img className='ml-2' src="/images/cart.png" alt="cart-icon" />
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   )
 }
 
-export default Navigation
+export default Navigation;

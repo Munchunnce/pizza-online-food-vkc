f6c39787d42688f0c4e2c14905a5fcf246b63382
Vimal <vimalkumarak478@gmail.com>
2025-09-27 23:33:05 +0530

    products page all data fetch vkc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import { BrowserRouter, Route, Routes,  } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
-import Products from './pages/Products';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
+import AllProducts from './pages/AllProducts';
 
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <Navigation/>
         <Routes>
           <Route path='/' exact element={<Home/>}></Route>
-          <Route path='/products' element={<Products/>}></Route>
+          <Route path='/products' element={<AllProducts/>}></Route>
           <Route path='/register' element={<Register/>}></Route>
           <Route path='/login' element={<Login/>}></Route>
           <Route path='/cart' element={<Cart/>}></Route>

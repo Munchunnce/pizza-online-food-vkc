adae7d06054c5f361aebe6aa2caf4deafff8162b
Vimal <vimalkumarak478@gmail.com>
2025-09-28 02:28:47 +0530

    UI fix vkc

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Product = (props) => {
     const { product } = props;
   return (
-    <div>
-        <img src={product.image} alt="pizza" />
-        <div className='text-center'>
-            <h2 className='text-large font-bold py-2'>{product.name}</h2>
-            <span className='bg-gray-200 py-1 rounded-full text-sm px-4'>{product.size}</span>
-        </div>
-        <div className='flex justify-between items-center mt-4'>
-            <span className='font-bold'>₹ {product.price}</span>
-            <button className='bg-white border border-[#FE5F1E] text-[#FE5F1E] hover:bg-[#FE5F1E] hover:text-white py-1 px-4 rounded-full font-bold cursor-pointer transition-colors duration-200'><span className='mr-4'>+</span>ADD</button>
-        </div>
-    </div>
+    <Link to={`/products/${product._id}`}>
+      <div>
+          <img className="w-59 h-59 object-cover rounded-lg" src={product.image} alt="pizza" />
+          <div className='text-center'>
+              <h2 className='text-large font-bold py-2'>{product.name}</h2>
+              <span className='bg-gray-200 py-1 rounded-full text-sm px-4'>{product.size}</span>
+          </div>
+          <div className='flex justify-between items-center mt-4'>
+              <span className='font-bold'>₹ {product.price}</span>
+              <button className='bg-white border border-[#FE5F1E] text-[#FE5F1E] hover:bg-[#FE5F1E] hover:text-white py-1 px-4 rounded-full font-bold cursor-pointer transition-colors duration-200'><span className='mr-4'>+</span>ADD</button>
+          </div>
+      </div>
+    </Link>
   )
 }
 
--- frontend/src/pages/SingleProducts.jsx
+++ frontend/src/pages/SingleProducts.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
 
 const SingleProducts = () => {
+    const [product, setProduct] = useState({});
+    const params = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetch(`/api/products/${params._id}`)
+        .then(res => res.json())
+        .then(product => {
+            setProduct(product);
+        })
+    }, [params._id]);
   return (
-    <div>
-      This is SingleProduct.
+    <div className='container mx-auto mt-12'>
+        <button onClick={() => navigate('/')} className='mb-12 bg-white border border-[#FE5F1E] text-[#FE5F1E] hover:bg-[#FE5F1E] hover:text-white py-1 px-4 rounded-full font-bold cursor-pointer transition-colors duration-200'>Back</button>
+       <div className='flex justify-center'>
+            <img className="w-80 h-80 object-cover rounded-lg" src={product.image} alt="pizza" />
+            <div className='ml-16'>
+                <h1 className='text-xl font-bold'>{product.name}</h1>
+                <div className='text-md text-gray-500'>{product.size}</div>
+                <div className='font-bold mt-2'>₹ {product.price}</div>
+                <button className='bg-white border border-[#FE5F1E] text-[#FE5F1E] hover:bg-[#FE5F1E] hover:text-white py-1 px-4 mt-4 rounded-full font-bold cursor-pointer transition-colors duration-200'>Add to cart</button>
+            </div>
+       </div>
     </div>
   )
 }

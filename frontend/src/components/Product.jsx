66902e6a2b5dedfd3884a6182a50fdd14144e356
Vimal <vimalkumarak478@gmail.com>
2025-09-27 23:25:14 +0530

    products page button color change vkc

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -6,12 +6,12 @@ const Product = (props) => {
     <div>
         <img src={product.image} alt="pizza" />
         <div className='text-center'>
-            <h2 className='text-large font-bold py-2'>{product.title}</h2>
+            <h2 className='text-large font-bold py-2'>{product.name}</h2>
             <span className='bg-gray-200 py-1 rounded-full text-sm px-4'>{product.size}</span>
         </div>
         <div className='flex justify-between items-center mt-4'>
-            <span>₹ {product.price}</span>
-            <button className='bg-yellow-500 py-1 px-4 rounded-full font-bold cursor-pointer'>ADD</button>
+            <span className='font-bold'>₹ {product.price}</span>
+            <button className='bg-white border border-[#FE5F1E] text-[#FE5F1E] hover:bg-[#FE5F1E] hover:text-white py-1 px-4 rounded-full font-bold cursor-pointer transition-colors duration-200'><span className='mr-4'>+</span>ADD</button>
         </div>
     </div>
   )

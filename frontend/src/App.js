d86bb7bae92f9a1496dee343866e29a25736d2c7
Vimal <vimalkumarak478@gmail.com>
2025-09-29 03:54:57 +0530

    footer page create file and route  vkc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,22 +9,26 @@ import Cart from './pages/Cart';
 import ProductsPage from './pages/ProductsPage';
 import SingleProducts from './pages/SingleProducts';
 import store from './store/store';
+import Footer from './pages/Footer';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col min-h-screen">
       <Provider store={store}>
         <BrowserRouter>
         <Navigation/>
-          <Routes>
-            <Route path='/' exact element={<Home/>}></Route>
-            <Route path='/register' element={<Register/>}></Route>
-            <Route path='/login' element={<Login/>}></Route>
-            <Route path='/products' exact element={<ProductsPage/>}></Route>
-            <Route path='/products/:_id' element={<SingleProducts/>}></Route>
-            <Route path='/cart' element={<Cart/>}></Route>
-          </Routes>
+          <div className="flex-1">
+            <Routes>
+              <Route path='/' exact element={<Home/>}></Route>
+              <Route path='/register' element={<Register/>}></Route>
+              <Route path='/login' element={<Login/>}></Route>
+              <Route path='/products' exact element={<ProductsPage/>}></Route>
+              <Route path='/products/:_id' element={<SingleProducts/>}></Route>
+              <Route path='/cart' element={<Cart/>}></Route>
+            </Routes>
+          </div>
+          <Footer/>
         </BrowserRouter>
       </Provider>
     </div>

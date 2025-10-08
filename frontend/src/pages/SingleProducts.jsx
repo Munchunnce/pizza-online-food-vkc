4a0aaf89147655eaca0ec4a97a66a7a597fc9fb7
Vimal <vimalkumarak478@gmail.com>
2025-10-09 00:10:51 +0530

    img, bg, fix  singleProduct update vkc

--- frontend/src/pages/SingleProducts.jsx
+++ frontend/src/pages/SingleProducts.jsx
@@ -37,10 +37,10 @@ const handleAddToCart = (e) => {
         )
 
       }
-      <div className='container mx-auto mt-12'>
+      <div className='container bg-[#F8F8F8] mx-auto mt-9 rounded-xl'>
         <button onClick={() => navigate('/')} className='mb-12 bg-white border border-[#FE5F1E] text-[#FE5F1E] hover:bg-[#FE5F1E] hover:text-white py-1 px-4 rounded-full font-bold cursor-pointer transition-colors duration-200'>Back</button>
-        <div className='flex justify-center'>
-          <img className="w-80 h-80 object-cover rounded-lg" src={product.image} alt="pizza" />
+        <div className='flex justify-center shadow-md rounded-xl hover:shadow-lg transition duration-300'>
+          <img className="w-70 h-70 object-cover rounded-lg" src={product.image} alt="pizza" />
           <div className='ml-16'>
             <h1 className='text-xl font-bold'>{product.name}</h1>
             <div className='text-md text-gray-500'>{product.size}</div>

54b3236973c10dd7f6b0f491aa78a46fb179b2b3
Vimal Kumar Chaudhary <112014356+Munchunnce@users.noreply.github.com>
2026-02-23 14:30:26 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,6 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
 
-
 //  Stripe public key
 const stripePromise = loadStripe(
   "pk_test_51SDlgPRz3ObFp51Jl6MYMamJjAAtFf03juq4FEQCSQuq1l2lJPQvjktWk6YZETIxySNgc1fZyg18mhchrJLRXzO000K1k53Qp7"
